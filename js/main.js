3bbbc5c6c40de6c03e8c958968a48e6d37879f32
kagesaburo27 <70998876+kagesaburo27@users.noreply.github.com>
2021-08-20 19:31:51 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
 $(document).ready(function () {
     var currentFloor = 2;
-    var floorPath = $('.home-image path');
+    var floorPath = $(".home-image path");
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+    var flatPath = $(".flats path");
+    var flatDesc = $(".flat-link");
+
+    flatPath.on("mouseover", function(){
+        flatPath.removeClass("current-flat");
+        flatDesc.removeClass("flat-hover");
+        currentFlat = $(this).attr('data-flat-img');
+        $(`[data-flat='${currentFlat}']`).toggleClass("flat-hover");
+    });
+
+    flatDesc.on("mouseover", function(){
+        flatPath.removeClass("current-flat");
+        flatDesc.removeClass("flat-hover");
+        currentFlatImg = $(this).attr('data-flat');
+        $(`[data-flat-img='${currentFlatImg}']`).toggleClass("current-flat");
+    });
+
+
     floorPath.on("mouseover", function(){
         floorPath.removeClass("current-floor");
         currentFloor = $(this).attr('data-floor');
         $('.counter').text(currentFloor);
     });
+
+    floorPath.on('click', toggleModal);
+
+    modalCloseButton.on('click', toggleModal);
+
+    viewFlatsButton.on('click', toggleModal);
 
     counterUp.on("click", function() {
         if(currentFloor<18) {
@@ -28,5 +55,7 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
     })
-
+    function toggleModal() {
+         modal.toggleClass("is-open");
+    }
 });
